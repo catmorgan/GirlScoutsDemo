1ff07fce6369cc1d8698c9538bddd9c421278087
Catherine Morgan <morgcat11@gmail.com>
2017-10-19 09:56:17 -0700

    girl scout theme

--- scripts/modules/header.js
+++ scripts/modules/header.js
@@ -6,12 +6,8 @@ export default class Header extends React.Component{
   render(){
     return (
       <section className="site-header">
-        <div className="header-menu">
-          <a className="fa fa-github" aria-hidden="true" href="https://github.com/catmorgan"/>
-        </div>
         <div className="header-content">
-          <h1>Hey, I'm Cat</h1>
-          <h2>I'm a software engineer.</h2>
+          <h1>Welcome to Blizzard, Girl Scouts!</h1>
         </div>
       </section>
     );
--- scripts/modules/site.js
+++ scripts/modules/site.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header';
-import About from './about';
 
 export default class Site extends React.Component{
 
   render(){
     return (
-      <div>
+      <div className="react-container">
         <Header/>
-        <About/>
       </div>
     );
   }
